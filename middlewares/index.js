3298e1bd94dd5622cf54e4ca0ed133ea5d314175
itsreimau <private.iblmln@gmail.com>
2026-09-13 10:14:22 +0700

    chore: update package version and remove unused middleware

--- middlewares/index.js
+++ middlewares/index.js
@@ -1,9 +1,7 @@
 const Check = require("./check");
-const Track = require("./track");
 const Validate = require("./validate");
 
 module.exports = (bot) => {
     Check(bot);
-    Track(bot);
     Validate(bot);
 };
